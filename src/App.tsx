7e5eca6f0182994fd963ebe93f74e871cd950392
cambrown101 <cameron-brown@live.com>
2021-03-26 18:08:36 -0700

    Add carasouel test component

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,13 @@
 import React from 'react';
 import './App.scss';
 
+import CarouselTest from './components/CarouselTest';
+
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
+      <CarouselTest />
     </div>
   );
 }
