49c371c7e8add8975b5c71a1a27b6093f619f054
cambrown101 <cameron-brown@live.com>
2021-03-26 18:09:00 -0700

    Set up carasouel

---
+++ src/components/CarouselTest.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import './CarouselTest.scss';
+import 'react-responsive-carousel/lib/styles/carousel.min.css';
+import { Carousel } from 'react-responsive-carousel';
+
+const CarouselTest = () => {
+  return (
+    <Carousel
+      interval={2000}
+      autoPlay={true}
+      centerMode={true}
+      infiniteLoop={true}>
+      <div className="one"></div>
+      <div className="two"></div>
+      <div className="three"></div>
+    </Carousel>
+  );
+};
+
+export default CarouselTest;
