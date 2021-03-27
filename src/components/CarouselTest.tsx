aca92a81c11ae30d1920c86f4d233b4fcbf561b6
cambrown101 <cameron-brown@live.com>
2021-03-26 19:13:28 -0700

    Remove carousel

--- src/components/CarouselTest.tsx
+++ src/components/CarouselTest.tsx
@@ -5,17 +5,19 @@ import { Carousel } from 'react-responsive-carousel';
 
 const CarouselTest = () => {
   return (
-    <Carousel
-      interval={10000}
-      autoPlay={true}
-      centerMode={true}
-      infiniteLoop={true}
-      showIndicators={false}
-      showStatus={false}>
-      <div className="one"></div>
-      <div className="two"></div>
-      <div className="three"></div>
-    </Carousel>
+    <div className="carousel-container">
+      <Carousel
+        interval={10000}
+        autoPlay={true}
+        centerMode={true}
+        infiniteLoop={true}
+        showIndicators={false}
+        showStatus={false}>
+        <div className="one"></div>
+        <div className="two"></div>
+        <div className="three"></div>
+      </Carousel>
+    </div>
   );
 };
 
