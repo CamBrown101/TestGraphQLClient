739e44aa7b7254f89031464e8fab6049c0542c21
cambrown101 <cameron-brown@live.com>
2021-03-28 12:51:46 -0700

    remove carasouel

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,10 @@
 import React from 'react';
 import './App.scss';
 
-import CarouselTest from './components/CarouselTest';
-
 function App() {
   return (
     <div className="App">
       <h1>Hello</h1>
-      <CarouselTest />
     </div>
   );
 }
