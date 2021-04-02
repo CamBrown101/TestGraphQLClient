83242b99947e2c3e61f13dce935cdf96af39fb45
cambrown101 <cameron-brown@live.com>
2021-04-01 22:24:02 -0700

    Move circle logic to slide component

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,8 +1,6 @@
 import React, { useState, useEffect } from 'react';
 import CarouselSlide from './CarouselSlide';
 import styled from 'styled-components';
-import { Circle } from '@styled-icons/bootstrap/Circle';
-import { CircleFill } from '@styled-icons/bootstrap/CircleFill';
 
 type Props = {
   interval: number;
@@ -37,33 +35,33 @@ const Carousel = ({ interval, numberOfSlides }: Props) => {
   });
 
   const slidesArray = [
-    <CarouselSlide slide={slide} color="pink" />,
-    <CarouselSlide slide={slide} color="red" />,
-    <CarouselSlide slide={slide} color="blue" />,
-    <CarouselSlide slide={slide} color="green" />,
-    <CarouselSlide slide={slide} color="black" />,
+    <CarouselSlide
+      numberOfSlides={numberOfSlides}
+      slide={slide}
+      color="pink"
+    />,
+    <CarouselSlide numberOfSlides={numberOfSlides} slide={slide} color="red" />,
+    <CarouselSlide
+      numberOfSlides={numberOfSlides}
+      slide={slide}
+      color="blue"
+    />,
+    <CarouselSlide
+      numberOfSlides={numberOfSlides}
+      slide={slide}
+      color="green"
+    />,
+    <CarouselSlide
+      numberOfSlides={numberOfSlides}
+      slide={slide}
+      color="black"
+    />,
   ];
-
-  const createCircles = (numberOfSlides: number, slideNumber: number) => {
-    const arrayOfCircles: any[] = [];
-
-    for (let i = 1; i <= numberOfSlides; i++) {
-      if (i === slideNumber) {
-        arrayOfCircles.push(<CircleFill className="circle-fill" key={i} />);
-      } else {
-        arrayOfCircles.push(<Circle className="circle" key={i} />);
-      }
-    }
-    return arrayOfCircles;
-  };
 
   return (
     <CarouselContainer>
-      <p>{slide}</p>
       {slidesArray[slide - 1]}
-      <div className="circle-container">
-        {createCircles(numberOfSlides, slide)}
-      </div>
+
       <Buttons onClick={() => previousSlide()}>last</Buttons>
       <Buttons primary="white" onClick={() => nextSlide()}>
         next
--- src/components/Carousel/CarouselSlide.tsx
+++ src/components/Carousel/CarouselSlide.tsx
@@ -1,15 +1,36 @@
 import React from 'react';
 import styled from 'styled-components';
+import { Circle } from '@styled-icons/bootstrap/Circle';
+import { CircleFill } from '@styled-icons/bootstrap/CircleFill';
 
 type Props = {
   color: string;
   slide: number;
+  numberOfSlides: number;
 };
 
-const CarouselSlide = ({ color, slide }: Props) => {
+const CarouselSlide = ({ color, slide, numberOfSlides }: Props) => {
+  const createCircles = (numberOfSlides: number, slideNumber: number) => {
+    const arrayOfCircles: any[] = [];
+
+    for (let i = 1; i <= numberOfSlides; i++) {
+      if (i === slideNumber) {
+        arrayOfCircles.push(<CircleFill className="circle-fill" key={i} />);
+      } else {
+        arrayOfCircles.push(<Circle className="circle" key={i} />);
+      }
+    }
+    return arrayOfCircles;
+  };
+
   return (
     <SlideWrapper color={color}>
-      <div className="slideBackground">{slide}</div>
+      <div className="slideBackground">
+        <div className="circle-container">
+          <p>{slide}</p>
+        </div>
+        {createCircles(numberOfSlides, slide)}
+      </div>
     </SlideWrapper>
   );
 };
