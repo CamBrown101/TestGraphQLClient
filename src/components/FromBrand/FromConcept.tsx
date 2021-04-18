88e0bfdbb86c6b6b046edbe7d06053e1a5a233cc
cambrown101 <cameron-brown@live.com>
2021-04-18 12:40:38 -0700

    Rough out FromConcept Section

---
+++ src/components/FromBrand/FromConcept.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import styled from "styled-components";
+
+const FromConcept = () => {
+  return (
+    <div>
+      FROM CONCEPT TO CONSUMER SERVICES Our services combined with your
+      exceptional product will build your customer base and keep them coming
+      back. Website Design and Architecture Online Store Setup Business
+      Strategies Search Engine Optimization and Search Engine Marketing Strategy
+      Management • Social Media Campaigns Liquor Branding TOOLS Fully optimized
+      Shopify Template Google Analytics Pixels / social media ads Retargeting
+      Cart abandonment emails ontent Creation
+    </div>
+  );
+};
+
+export default FromConcept;
+
+const FromBrandContainer = styled.section`
+  background-color: #fff;
+`;
