dbaa0780c135fcb5c8191fd0d6dbe97a631f229d
cambrown101 <cameron-brown@live.com>
2021-04-18 13:03:01 -0700

    Add font

--- src/App.tsx
+++ src/App.tsx
@@ -1,26 +1,32 @@
-import React from 'react';
-import './App.scss';
+import React from "react";
+import "./App.scss";
 import {
   Redirect,
   BrowserRouter as Router,
   Route,
   Switch,
-} from 'react-router-dom';
-
-import Carousel from './components/Carousel/Carousel';
+} from "react-router-dom";
+import styled from "styled-components";
+import Carousel from "./components/Carousel/Carousel";
 
 function App() {
   return (
-    <Router>
-      <Switch>
-        <Route
-          path="/"
-          exact
-          component={() => <Carousel interval={3000} numberOfSlides={5} />}
-        />
-      </Switch>
-    </Router>
+    <Wrapper>
+      <Router>
+        <Switch>
+          <Route
+            path="/"
+            exact
+            component={() => <Carousel interval={3000} numberOfSlides={5} />}
+          />
+        </Switch>
+      </Router>
+    </Wrapper>
   );
 }
 
 export default App;
+
+const Wrapper = styled.div`
+  @import url("https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;500;600;700;900&display=swap");
+`;
