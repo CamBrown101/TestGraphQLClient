dfc609357fe4c1a9419d60fa12b2219ff5d6b699
cambrown101 <cameron-brown@live.com>
2021-04-18 13:03:32 -0700

    rough out html

--- src/components/FromBrand/FromConcept.tsx
+++ src/components/FromBrand/FromConcept.tsx
@@ -3,20 +3,47 @@ import styled from "styled-components";
 
 const FromConcept = () => {
   return (
-    <div>
-      FROM CONCEPT TO CONSUMER SERVICES Our services combined with your
-      exceptional product will build your customer base and keep them coming
-      back. Website Design and Architecture Online Store Setup Business
-      Strategies Search Engine Optimization and Search Engine Marketing Strategy
-      Management • Social Media Campaigns Liquor Branding TOOLS Fully optimized
-      Shopify Template Google Analytics Pixels / social media ads Retargeting
-      Cart abandonment emails ontent Creation
-    </div>
+    <FromBrandContainer>
+      <Titles>
+        <h2>FROM CONCEPT</h2>
+        <h2>TO CONSUMER</h2>
+      </Titles>
+      <h3>
+        Our services combined with your exceptional product will build your
+        customer base and keep them coming back.
+      </h3>
+      <h3>SERVICES</h3>
+      <ul>
+        <li>Website Design and Architecture</li>
+        <li>Online Store Setup Business</li>
+        <li>
+          Strategies Search Engine Optimization and Search Engine Marketing
+        </li>
+        <li>Strategy Management</li>
+        <li>Social Media Campaigns</li>
+        <li>Liquor Branding</li>
+      </ul>
+      <h3>TOOLS</h3>
+      <ul>
+        <li>Fully optimized Shopify Template</li>
+        <li>Google Analytics</li>
+        <li>Pixels / social media ads</li>
+        <li>Retargeting</li>
+        <li>Cart abandonment emails</li>
+        <li>Content Creation</li>
+      </ul>
+    </FromBrandContainer>
   );
 };
 
 export default FromConcept;
 
 const FromBrandContainer = styled.section`
+  margin: 0;
+  width: 100%;
   background-color: #fff;
+`;
+const Titles = styled.div`
+  display: flex;
+  justify-content: space-between;
 `;
