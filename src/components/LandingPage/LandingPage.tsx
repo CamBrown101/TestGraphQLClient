4f47ac66f8da897f09ea1b6c9075c30e32daaecc
cambrown101 <cameron-brown@live.com>
2021-04-18 19:35:40 -0700

    Create a page to render components together

---
+++ src/components/LandingPage/LandingPage.tsx
@@ -0,0 +1,14 @@
+import React from "react";
+import TopSection from "../TopSection/TopSection";
+import FromConcept from "../FromConcept/FromConcept";
+
+const LandingPage = () => {
+  return (
+    <div>
+      <TopSection />
+      <FromConcept />
+    </div>
+  );
+};
+
+export default LandingPage;
