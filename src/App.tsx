e4223f9fe29d4b5c9e93ba69458ca67eaaac1d4e
cambrown101 <cameron-brown@live.com>
2021-04-20 18:00:32 -0700

    Fix Imports

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import styled from "styled-components";
 import LandingPage from "./components/LandingPage/LandingPage";
 import WhyAcclamation from "./components/WhyAcclamation/WhyAcclamation";
-import Mission from "./components/Mission/Mission";
+import HowWeWork from "./components/HowWeWork/HowWeWork";
 
 function App() {
   return (
@@ -11,7 +11,7 @@ function App() {
       <Switch>
         <Route path="/" exact component={LandingPage} />
         <Route path="/Why-Acclamation" exact component={WhyAcclamation} />
-        <Route path="/Mission" exact component={Mission} />
+        <Route path="/How-We-Work" exact component={HowWeWork} />
       </Switch>
     </Router>
   );
