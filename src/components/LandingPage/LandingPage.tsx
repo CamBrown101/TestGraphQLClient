202bdc8e2c702adcfd49379dd15702f05e8be562
cambrown101 <cameron-brown@live.com>
2021-04-20 18:38:38 -0700

    Add carousel

--- src/components/LandingPage/LandingPage.tsx
+++ src/components/LandingPage/LandingPage.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import TopSection from "../TopSection/TopSection";
 import FromConcept from "../FromConcept/FromConcept";
+import Carousel from "../Carousel/Carousel";
 
 const LandingPage = () => {
   return (
     <div>
       <TopSection />
       <FromConcept />
+      <Carousel interval={2000} numberOfSlides={4} />
     </div>
   );
 };
