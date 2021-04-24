ec432ea197b266a8096487380356b7e61b021e13
cambrown101 <cameron-brown@live.com>
2021-04-24 14:38:10 -0700

    Fix react error

--- src/components/Carousel/Carousel.tsx
+++ src/components/Carousel/Carousel.tsx
@@ -1,4 +1,4 @@
-import { useState, useEffect } from "react";
+import React, { useState, useEffect } from "react";
 import CarouselSlide from "./CarouselSlide";
 import People from "./PeopleSlides/PeopleSlide";
 import styled from "styled-components";
