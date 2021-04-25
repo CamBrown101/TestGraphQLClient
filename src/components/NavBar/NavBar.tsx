a5ec41f54250912fa079a12882728d8aaecce9c1
cambrown101 <cameron-brown@live.com>
2021-04-24 18:38:28 -0700

    Style out Nav

--- src/components/NavBar/NavBar.tsx
+++ src/components/NavBar/NavBar.tsx
@@ -9,20 +9,31 @@ const NavBar = () => {
       <NavLink className="nav-menu-item" id="nav-menu-home" exact to="/">
         <img className="logo" alt="Acclamation Logo" src={Logo}></img>
       </NavLink>
-      <NavLink
-        className="nav-menu-item"
-        id="nav-menu-why-acclamation"
-        exact
-        to="/Why-Acclamation">
-        How We Work
-      </NavLink>
-      <NavLink
-        className="nav-menu-item"
-        id="nav-menu-how-we-work"
-        exact
-        to="/How-We-Work">
-        How We Work
-      </NavLink>
+      <div className="nav-inner-container">
+        <div className="nav-btns">
+          <NavLink
+            className="nav-menu-item"
+            id="nav-menu-why-acclamation"
+            exact
+            to="/Why-Acclamation">
+            How We Work
+          </NavLink>
+          <NavLink
+            className="nav-menu-item"
+            id="nav-menu-how-we-work"
+            exact
+            to="/How-We-Work">
+            How We Work
+          </NavLink>
+        </div>
+        <NavLink
+          className="nav-menu-item"
+          id="nav-menu-evolve-with-us"
+          exact
+          to="https://forms.gle/ZaZhfMSeV9QxgnNcA">
+          Evolve With Us
+        </NavLink>
+      </div>
     </NavBarWrapper>
   );
 };
@@ -32,9 +43,14 @@ export default NavBar;
 const NavBarWrapper = styled.nav`
   position: fixed;
   top: 0;
+  display: flex;
+  align-items: center;
   width: 100%;
   padding: 15px 2% 10px;
   background-color: black;
+  border-bottom: 1px solid #463c30;
+  color: white;
+
   .logo {
     height: 70px;
     padding-right: 20px;
@@ -42,5 +58,43 @@ const NavBarWrapper = styled.nav`
     border-right: 1px solid #424242;
     filter: invert(15%) sepia(6%) saturate(1341%) hue-rotate(353deg)
       brightness(83%) contrast(84%);
+  }
+  .nav-btns {
+    margin-top: 15px;
+  }
+  .nav-inner-container {
+    width: 100%;
+    margin-right: 2%;
+    display: flex;
+    justify-content: space-between;
+  }
+  a,
+  a:hover,
+  a:focus,
+  a:active {
+    text-decoration: none;
+    color: inherit;
+    transition: color 0.8s;
+  }
+  a:hover {
+    color: #b09a7f;
+  }
+  #nav-menu-why-acclamation,
+  #nav-menu-how-we-work,
+  #nav-menu-evolve-with-us {
+    margin-right: 25px;
+    font-family: "Work Sans", sans-serif;
+    font-weight: 900;
+    font-size: 15px;
+    letter-spacing: 1px;
+    text-transform: uppercase;
+  }
+  #nav-menu-evolve-with-us {
+    margin-top: 15px;
+    color: #b09a7f;
+    transition: color 0.8s;
+  }
+  #nav-menu-evolve-with-us:hover {
+    color: white;
   }
 `;
