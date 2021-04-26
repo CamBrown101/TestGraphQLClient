a4ac04452fe776bfc3275fa37eeaa1f9f615916a
cambrown101 <cameron-brown@live.com>
2021-04-25 19:14:32 -0700

    Add ReachControlStablity component

--- src/components/WhyAcclamation/WhyAcclamation.tsx
+++ src/components/WhyAcclamation/WhyAcclamation.tsx
@@ -1,12 +1,14 @@
 import React from "react";
 import styled from "styled-components";
 import WhyHeader from "./WhyAcclamationComponents/WhyHeader";
+import ReachControlStability from "./WhyAcclamationComponents/ReachControlScalability";
 import Footer from "../Footer/Footer";
 
 const WhyAcclamation = () => {
   return (
     <WhyAcclamationWrapper>
       <WhyHeader />
+      <ReachControlStability />
       <Footer />
     </WhyAcclamationWrapper>
   );
