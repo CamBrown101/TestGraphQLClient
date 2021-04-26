ca6c38a7efdd2a5bee46ad1d9efa9bd993fd6fa3
cambrown101 <cameron-brown@live.com>
2021-04-25 17:30:41 -0700

    Fix max width

--- src/components/FromConcept/FromConcept.tsx
+++ src/components/FromConcept/FromConcept.tsx
@@ -4,49 +4,51 @@ import styled from "styled-components";
 const FromConcept = () => {
   return (
     <FromBrandContainer>
-      <Titles>
-        <h2 className="title-one">FROM CONCEPT</h2>
-        <h2 className="title-two">TO CONSUMER</h2>
-      </Titles>
-      <h3>
-        Our services combined with your exceptional product will build your
-        customer base and keep them coming back.
-      </h3>
-      <ListContainer>
-        <Lists>
-          <div className="list-titles">
-            <img
-              src="https://assets.website-files.com/5ece8808610c986cbcec2b27/5f2013fb6e9d964b1d4ca673_Speakeasy_CO-Icon-Online-Store.svg"
-              alt="Services"
-              className="image"></img>
-            <h5>SERVICES</h5>
-          </div>
-          <li>Website Design and Architecture</li>
-          <li>Online Store Setup Business</li>
-          <li>
-            Strategies Search Engine Optimization and Search Engine Marketing
-          </li>
-          <li>Strategy Management</li>
-          <li>Social Media Campaigns</li>
-          <li>Liquor Branding</li>
-        </Lists>
-        <Lists>
-          <div className="list-titles">
-            <img
-              src="https://assets.website-files.com/5ece8808610c986cbcec2b27/5ece9faf50659013ef0a6f2b_Speakeasy_CO-Icon-Control.svg"
-              alt="Tools"
-              className="image"></img>
-            <h5>Tools</h5>
-          </div>
-          <li>Fully optimized Shopify Template</li>
-          <li>Google Analytics</li>
-          <li>Pixels / social media ads</li>
-          <li>Retargeting</li>
-          <li>Cart abandonment emails</li>
-          <li>Content Creation</li>
-        </Lists>
-      </ListContainer>
-      <Button>How We Work</Button>
+      <FromBrandInnerContainer>
+        <Titles>
+          <h2 className="title-one">FROM CONCEPT</h2>
+          <h2 className="title-two">TO CONSUMER</h2>
+        </Titles>
+        <h3>
+          Our services combined with your exceptional product will build your
+          customer base and keep them coming back.
+        </h3>
+        <ListContainer>
+          <Lists>
+            <div className="list-titles">
+              <img
+                src="https://assets.website-files.com/5ece8808610c986cbcec2b27/5f2013fb6e9d964b1d4ca673_Speakeasy_CO-Icon-Online-Store.svg"
+                alt="Services"
+                className="image"></img>
+              <h5>SERVICES</h5>
+            </div>
+            <li>Website Design and Architecture</li>
+            <li>Online Store Setup Business</li>
+            <li>
+              Strategies Search Engine Optimization and Search Engine Marketing
+            </li>
+            <li>Strategy Management</li>
+            <li>Social Media Campaigns</li>
+            <li>Liquor Branding</li>
+          </Lists>
+          <Lists>
+            <div className="list-titles">
+              <img
+                src="https://assets.website-files.com/5ece8808610c986cbcec2b27/5ece9faf50659013ef0a6f2b_Speakeasy_CO-Icon-Control.svg"
+                alt="Tools"
+                className="image"></img>
+              <h5>Tools</h5>
+            </div>
+            <li>Fully optimized Shopify Template</li>
+            <li>Google Analytics</li>
+            <li>Pixels / social media ads</li>
+            <li>Retargeting</li>
+            <li>Cart abandonment emails</li>
+            <li>Content Creation</li>
+          </Lists>
+        </ListContainer>
+        <Button>How We Work</Button>
+      </FromBrandInnerContainer>
     </FromBrandContainer>
   );
 };
@@ -56,8 +58,6 @@ export default FromConcept;
 const FromBrandContainer = styled.section`
   /* @import url("https://fonts.googleapis.com/css2?family=Work+Sans:wght@400;500;600;700;900&display=swap"); */
   background-color: #fff;
-  margin-left: 2%;
-  margin-right: 2%;
   display: flex;
   flex-direction: column;
   align-items: center;
@@ -69,6 +69,11 @@ const FromBrandContainer = styled.section`
     line-height: 40px;
   }
 `;
+const FromBrandInnerContainer = styled.div`
+  max-width: 1400px;
+  width: 90%;
+`;
+
 const Titles = styled.div`
   display: flex;
   justify-content: space-between;
