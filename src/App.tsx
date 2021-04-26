12b91ea9f604113adb67e962d6c93523d9f761bb
cambrown101 <cameron-brown@live.com>
2021-04-25 18:52:11 -0700

    Fix path error

--- src/App.tsx
+++ src/App.tsx
@@ -3,7 +3,7 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import NavBar from "./components/NavBar/NavBar";
 import LandingPage from "./components/LandingPage/LandingPage";
 import WhyAcclamation from "./components/WhyAcclamation/WhyAcclamation";
-import HowWeWork from "./components/HowWeWork/HowWeWork";
+import HowWeWork from "./components/LandingPage/LandingComponents/HowWeWork/HowWeWork";
 import PrivacyPolicy from "./components/PrivacyPolicy/PrivacyPolicy";
 
 function App() {
