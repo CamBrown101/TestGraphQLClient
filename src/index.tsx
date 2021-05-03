cd64c55a0f3d074002a9dc73a31f1c8af084fc3b
cambrown101 <cameron-brown@live.com>
2021-05-03 12:23:34 -0700

    Import Applo and grphql

--- src/index.tsx
+++ src/index.tsx
@@ -3,6 +3,26 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
 import "typeface-work-sans";
+import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+
+const client = new ApolloClient({
+  uri: "http://localhost:4000/",
+  cache: new InMemoryCache(),
+});
+
+client
+  .query({
+    query: gql`
+      {
+        slides {
+          header
+          title
+          description
+        }
+      }
+    `,
+  })
+  .then((result) => console.log(result));
 
 ReactDOM.render(
   <React.StrictMode>
