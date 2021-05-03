cd442d2dda8e09f01938af8bafa824f70ea2ab64
cambrown101 <cameron-brown@live.com>
2021-05-03 12:54:02 -0700

    Remove commented code

--- src/index.tsx
+++ src/index.tsx
@@ -3,30 +3,18 @@ import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
 import "typeface-work-sans";
-import { ApolloClient, InMemoryCache, gql } from "@apollo/client";
+import { ApolloClient, InMemoryCache, ApolloProvider } from "@apollo/client";
 
 const client = new ApolloClient({
   uri: "http://localhost:4000/",
   cache: new InMemoryCache(),
 });
 
-client
-  .query({
-    query: gql`
-      {
-        slides {
-          header
-          title
-          description
-        }
-      }
-    `,
-  })
-  .then((result) => console.log(result));
-
 ReactDOM.render(
   <React.StrictMode>
-    <App />
+    <ApolloProvider client={client}>
+      <App />
+    </ApolloProvider>
   </React.StrictMode>,
   document.getElementById("root")
 );
